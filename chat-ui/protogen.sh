18ed6db2dfe61991b242af92c3a9ef99bc83e822
TriNguyen <tri.nguyen@yoppworks.com>
2020-06-26 16:54:52 -0400

    reorg *.proto files

--- chat-ui/protogen.sh
+++ chat-ui/protogen.sh
@@ -1,76 +1,28 @@
 #!/bin/bash
 
-mkdir -p ./src/_proto/cloudstate
-mkdir -p ./src/_proto/google/api
-
-echo "Compiling protobuf definitions"
+printf "Compiling protobuf definitions:\033[32;5m Cloudstate & Google\033[0m\n"
 
 OUT_DIR="./src/_proto"
 PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
 
-
-echo "Compile cloudstate entity key"
-protoc \
-    --proto_path="node_modules/cloudstate/proto/google/api/" \
-    --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
-    --js_out="import_style=commonjs,binary:${OUT_DIR}/google/api" \
-    --ts_out="service=grpc-web:${OUT_DIR}/google/api" \
-    -I node_modules/cloudstate/protoc/include \
-    node_modules/cloudstate/proto/google/api/httpbody.proto
+mkdir -p ${OUT_DIR}
 
 protoc \
-    --proto_path="node_modules/cloudstate/proto/google/api/" \
+    --proto_path="node_modules/cloudstate/proto" \
     --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
-    --js_out="import_style=commonjs,binary:${OUT_DIR}/google/api" \
-    --ts_out="service=grpc-web:${OUT_DIR}/google/api" \
-    node_modules/cloudstate/proto/google/api/http.proto
-
-protoc \
-    --proto_path="node_modules/cloudstate/proto/google/api/" \
-    --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
-    --js_out="import_style=commonjs,binary:${OUT_DIR}/google/api" \
-    --ts_out="service=grpc-web:${OUT_DIR}/google/api" \
-    -I ../cloudstate/protocols/frontend/ \
-    -I node_modules/cloudstate/protoc/include \
-    node_modules/cloudstate/proto/google/api/annotations.proto
-
-protoc \
-    --proto_path="node_modules/cloudstate/proto/cloudstate/" \
-    --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
-    --js_out="import_style=commonjs,binary:${OUT_DIR}/cloudstate" \
-    --ts_out="service=grpc-web:${OUT_DIR}/cloudstate" \
-    -I node_modules/cloudstate/protoc/include \
+    --js_out="import_style=commonjs,binary:${OUT_DIR}" \
+    --ts_out="${OUT_DIR}" \
+    node_modules/cloudstate/proto/google/api/httpbody.proto \
+    node_modules/cloudstate/proto/google/api/http.proto \
+    node_modules/cloudstate/proto/google/api/annotations.proto \
     node_modules/cloudstate/proto/cloudstate/entity_key.proto
 
-echo "Compiling Chat Service"
-protoc \
-  -I node_modules/cloudstate/proto/google/api/ \
-  -I node_modules/cloudstate/proto/cloudstate/ \
-  --include_imports \
-  --proto_path=node_modules/cloudstate/proto \
-  --proto_path=node_modules/cloudstate/protoc/include \
-  --descriptor_set_out=user-function.desc \
-  --proto_path=. \
-  chat.proto
+printf "Compiling protobuf definitions (Chat dependent services):\033[32;5m Friends & Presence\033[0m\n"
 
-echo "Compile Friends Service"
 protoc \
+    --proto_path=./chatapp_protos \
     --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
     --js_out="import_style=commonjs,binary:${OUT_DIR}" \
     --ts_out="service=grpc-web:${OUT_DIR}" \
-    --proto_path="../friends" \
-    -I node_modules/cloudstate/proto/google/api/ \
     -I node_modules/cloudstate/proto/ \
-    -I ../friends/node_modules/cloudstate/protoc/include \
-    ../friends/friends.proto
-
-echo "Compile Presence Service"
-protoc \
-    --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
-    --js_out="import_style=commonjs,binary:${OUT_DIR}" \
-    --ts_out="service=grpc-web:${OUT_DIR}" \
-    --proto_path="../presence" \
-    -I node_modules/cloudstate/proto/google/api/ \
-    -I node_modules/cloudstate/proto/ \
-    -I ../presence/node_modules/cloudstate/protoc/include \
-    ../presence/presence.proto
+    friends.proto presence.proto
